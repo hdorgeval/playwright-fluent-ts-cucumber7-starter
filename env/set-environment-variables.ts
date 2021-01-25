4ab184ffa733d21fbe92c98c88c2e0456fe1581d
hdorgeval <henri.dorgeval@outlook.fr>
2021-01-25 14:41:00 +0100

    chore(env): disable chalk colors

--- env/set-environment-variables.ts
+++ env/set-environment-variables.ts
@@ -1,4 +1,7 @@
-process.env['--logLevel'] = 'error';
 process.env['--foo'] = 'bar';
-process.env['--no-color'] = 'true';
 process.env['SimpleConsoleFormatter.printEnvelopes'] = 'false';
+
+// make error messages displayed within the html report readable
+// https://github.com/chalk/chalk#chalksupportscolor
+// --no-color
+process.env['FORCE_COLOR'] = '0';
